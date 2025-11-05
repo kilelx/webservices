225e7bd2838c794d2526b143acb7aee4f387e71d
kilelx <llx.kieran@gmail.com>
2025-11-05 09:25:44 +0100

    feat: setup swagger docs

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,21 @@ import express from 'express';
 import { NotFound } from './middlewares/not-found.middleware';
 import { Health } from './routes/health.route';
 import routes from './routes'
+import swaggerUi from "swagger-ui-express"
+import swaggerOptions from "../swagger/options"
 
 const app = express();
+const swaggerJSdoc = require("swagger-jsdoc")
+const specs = swaggerJSdoc(swaggerOptions);
 
 app.use(express.json())
 
 app.use('/health', Health);
 
 app.use('/api/v1', routes);
+
+// Route du Swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 // tout à la fin, donc si pas de requêtes interceptées avant = 404
 app.use(NotFound);
---
+++ swagger/options.ts
@@ -0,0 +1,14 @@
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'E-commerce API',
+      version: '1.0.0',
+      description: 'API RESTful en TS',
+    },
+    servers: [{ url: 'http://localhost:3000/api/v1' }],
+  },
+  apis: ['./src/**/*.ts'], // <= important: .ts et non .js
+};
+
+export default swaggerOptions
